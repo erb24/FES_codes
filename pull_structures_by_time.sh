dee3e4bbcbaf01ced95e43e61339cd29007b71de
Eric Beyerle <ebeyerle@uoregon.edu>
2018-03-29 13:59:38 -0700

    Fix bug in pull_structures_by_time.sh

--- pull_structures_by_time.sh
+++ pull_structures_by_time.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
 
-codesdir=/home/ebeyerle/Desktop/codes
+codesdir=~/codes
 mol='1UBQ' #Molecule
 nres=$(sed -n "2p" protname.txt)
 natoms=$(sed -n "4p" protname.txt)
-mult=$(( ${natoms} + 7 )) #Assumes the trajectory is GROMACS version 5; might be +6 for GROMACS 2016.
+mult=$(( ${natoms} + 7 ))
 for mode in `seq 1 5`
 do
   echo "$mode" > mode
@@ -18,28 +18,37 @@ do
   echo $ncoords > ncoords
   cp -v coords_${mode}.dat coords
   #Selcting an average conformation from an ensemble:
+
   f95 ${codesdir}/test_ensemble_finder.f95
   ./a.out
-  echo "1" > select.inp
-  gmx trjconv -f ${mol}.xtc -s ${mol}_pro1.tpr -fr frames_${mode}.ndx -o all.pdb < select.inp
-  cp -v all.pdb tmp
+
   mkdir -v mode_${mode}_states
+  counter=0
+  echo "1" > select.inp
+  cp -v frames_${mode}.ndx tmp.ndx
   while read line 
   do 
     counter=$(( ${counter} +1 )) 
     echo "$line"
     tline=$( echo "${line}*${mult}" | bc -l)
     echo "$tline"
-    sed -n "1,${tline}p" tmp >${mol}_${counter}.pdb
-    cp -v ${mol}_${counter}.pdb mode_${mode}_states/
-    sed -i "1,${tline}d" tmp
+    #sed -n "1,${tline}p" tmp >${mol}_${counter}.pdb
+    #cp -v ${mol}_${counter}.pdb mode_${mode}_states/
+    #sed -i "1,${tline}d" tmp
+    sed -n "1,$(( 1+ ${line} ))p" tmp.ndx > frames_${mode}_${counter}.ndx
+    cp -v frames_${mode}_${counter}.ndx mode_${mode}_states/
+    gmx trjconv -f ../../${mol}.xtc -s ../../${mol}_2.tpr -fr frames_${mode}_${counter}.ndx -o ${mol}_${counter}.pdb < select.inp
+    mv -v 1UBQ_${counter}.pdb mode_${mode}_states/
+    sed -i "2,$(( 1+ ${line} ))d" tmp.ndx
   done <"nframes_${mode}"
   rm -rfv tmp
+  rm -rfv tmp.ndx
   echo "$counter"
+  cp -v all.pdb tmp
 
   for state in `seq 1 ${counter}`
   do
-    echo "1 1" | gmx rmsf -f ${mol}_${state}.pdb -s ${mol}_pro1.tpr -ox avg_mode_${mode}_${state}.pdb -fit no
+    echo "1 1" | gmx rmsf -f mode_${mode}_states/${mol}_${state}.pdb -s ../../${mol}_2.tpr -ox mode_${mode}_states/avg_mode_${mode}_${state}.pdb -fit no
     rm -rfv ${mol}_${state}.pdb
   done
   
